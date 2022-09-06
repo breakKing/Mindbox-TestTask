4df6a594b6dad41932f069521e50c71e42fa6e59
Daniil-Laptop <dduhovny26@gmail.com>
2022-09-06 03:50:18 +0300

    Подготовка к юнит тестам

--- src/TestTask.Library/Common/DoubleExtensions.cs
+++ src/TestTask.Library/Common/DoubleExtensions.cs
@@ -1,10 +1,8 @@
-using System;
-
 namespace TestTask.Library.Common;
 
 internal static class DoubleExtensions
 {
-    private static readonly double ERROR = 1e-7;
+    internal static readonly double ERROR = 1e-7;
 
     internal static bool IsEqualToWithAnError(this double thisNumber, double otherNumber)
     {
